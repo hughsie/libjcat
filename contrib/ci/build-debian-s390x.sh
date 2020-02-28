c46d484d0c0d546db17d89b1b0422c7d6481c795
Mario Limonciello <mario.limonciello@dell.com>
2020-02-28 12:40:27 -0600

    Add more CI targets

--- contrib/ci/build-debian-s390x.sh
+++ contrib/ci/build-debian-s390x.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
+
+#evaluate using Debian's build flags
+eval "$(dpkg-buildflags --export=sh)"
+#filter out -Bsymbolic-functions
+export LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+
 export LC_ALL=C.UTF-8
 mkdir -p build && cd build
 rm * -rf
 meson .. \
-    --cross-file=../contrib/mingw64.cross \
+    --cross-file=../contrib/s390x.cross \
     -Dintrospection=false \
     -Dgpg=false \
     -Dgtkdoc=false \
     -Dtests=true $@
 ninja -v || bash
-wine reg add "HKEY_CURRENT_USER\Environment" /v PATH /d /usr/x86_64-w64-mingw32/sys-root/mingw/bin
-
 ninja test -v
 DESTDIR=/tmp/install-ninja ninja install
 cd ..
