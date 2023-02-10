28664aa297d2583bec92e5808798f7a21838419e
Joe Qian <joeqian@google.com>
2023-02-10 18:49:16 +0000

    Add some introspection
    Also rearrange files and rename some variables

--- libjcat/jcat-proof-bits-private.h
+++ libjcat/jcat-proof-bits-private.h
@@ -1,16 +1,20 @@
+/*
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2022 Joe Qian <joeqian@google.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
 #pragma once
 
 #include <json-glib/json-glib.h>
 
-/* count number of 1's set */
 guint
 jcat_bits_ones_count64(guint64 val);
 
-/* count number of trailing zeros */
 guint
 jcat_bits_trailing_zeros64(guint64 val);
 
-/* minimum number of bits required to represent number */
 guint
 jcat_bits_bit_length64(guint64 val);
 
@@ -21,7 +25,7 @@ void
 _g_set_byte_array(GByteArray **buf, GByteArray *buf_new);
 
 gboolean
-fu_byte_array_compare(GByteArray *buf1, GByteArray *buf2, GError **error);
+jcat_byte_array_compare(GByteArray *buf1, GByteArray *buf2, GError **error);
 
 gchar *
 jcat_hex_encode_string(GByteArray *buf);
